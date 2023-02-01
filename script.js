6f64a04e59cc29722f9a72f947b941f57fe803de
Jonathan Baliellas <j.baliellas@hotmail.com>
2023-02-01 05:19:33 -0300

    Update script.js
    Fighters list sort added.

--- script.js
+++ script.js
@@ -7,19 +7,31 @@ $(document).ready(function(){
 
         //Preenche a lista com todos os combatentes selecionados
         for(let i = 0; i < fighterNum; i++){
-            fightersList.push($(`input:checked:eq(${i})`).val());
+            let fighter = {name:'',pos:''};//Objeto combatente
+            fighter.name = $(`input:checked:eq(${i})`).val();//Atribui o nome
+            fighter.pos = Math.random();//Atribui valor aleatório para posição
+            fightersList.push(fighter);//Inclui combatente da lista
         }
+
+        //Ordena a lista de combatentes (iniciativa)
+        fightersList.sort(comparePos);
 
         //Preenche a lista de iniciativa
         for(let i = 0; i < fightersList.length; i++){
-            elementsList.push(document.createElement('li'));
-            elementsList[i].innerHTML = fightersList[i];
-            $('#ol-initiative').append(elementsList[i]);
-            // $('#ol-initiative').append("<li>Oi</li>");
+            elementsList.push(document.createElement('li'));//Cria um item da lista
+            elementsList[i].innerHTML = fightersList[i].name;//Insere o nome do combatente
+            $('#ol-initiative').append(elementsList[i]);//Inclui o item na iniciativa
         }
 
         //Mostra/Esconde texto de lista vazia
-        if(fighterNum === 0) $('#emptyList').show();
-        else $('#emptyList').hide();
+        if(fighterNum === 0) $('#emptyList').show();//Mostra lista vazia
+        else $('#emptyList').hide();//Esconde lista vazia
     })
+
+    //Compara as posições dos combatentes
+    function comparePos(a, b){
+        if(a.pos < b.pos) {return -1};
+        if(a.pos > b.pos) {return 1};
+        return 0;
+    }
 });
